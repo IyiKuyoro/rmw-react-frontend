e513c5e52962e6222bddc9e2001b49a9dbc0a44a
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-10-13 17:31:43 +0100

    chore(setup): setup webpack and react
    - install all loaders
    - add webpack config for different environments
    - setup babel
    
    [Finishes #161198153]
    [Finishes #161197988]

---
+++ webpack.dev.js
@@ -0,0 +1,32 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: false,
+            }
+          },
+        ],
+      }
+    ]
+  }
+});
